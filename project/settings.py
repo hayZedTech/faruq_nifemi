4393650a75aa7074f8d941e27eb4fe9162d695f5
Azeez Ololade <ololadeazeez.m@gmail.com>
2026-06-02 15:36:44 +0100

    updated static

--- project/settings.py
+++ project/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,7 +136,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+DEFAULT_FILE_STORAGE="django.files.FileSystemStorage"
+STATICFILES_STORAGE="whitenoise.storage.StaticFilesStorage"
+
+STORAGES={
+    "default":{
+        "BACKEND":"django.files.FileSystemStorage"
+    },
+    "staticfiles":{
+        "BACKEND":"whitenoise.storage.StaticFilesStorage"
+    }
+}
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS":
